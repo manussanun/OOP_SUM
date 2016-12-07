2259c232454fe94937cbafc950c1e1cad560e110
zenter <zenter@zenters-MacBook-Air.local>
2016-12-07 17:25:53 +0700

    edit position input

--- world.py
+++ world.py
@@ -7,31 +7,38 @@ class World:
         self.height = height
         self.state = 'start'
         self.ans = ''
-        self.ans_pos_x = width/2
+        self.sum_ans = 0
+        self.ans_pos_x = self.width/2
 
     def on_key_press(self, key, key_modifiers):
-        if self.state == 'game' and 48 <= key <= 57:
-            self.ans_pos_x -= 11
-            if key == 48:
-                self.ans += '0'
-            elif key == 49:
-                self.ans += '1'
-            elif key == 50:
-                self.ans += '2'
-            elif key == 51:
-                self.ans += '3'
-            elif key == 52:
-                self.ans += '4'
-            elif key == 53:
-                self.ans += '5'
-            elif key == 54:
-                self.ans += '6'
-            elif key == 55:
-                self.ans += '7'
-            elif key == 56:
-                self.ans += '8'
-            elif key == 57:
-                self.ans += '9'
+        if self.state == 'game':
+            if 48 <= key <= 57:
+                self.ans_pos_x -= 11
+                if key == 48:
+                    self.ans += '0'
+                elif key == 49:
+                    self.ans += '1'
+                elif key == 50:
+                    self.ans += '2'
+                elif key == 51:
+                    self.ans += '3'
+                elif key == 52:
+                    self.ans += '4'
+                elif key == 53:
+                    self.ans += '5'
+                elif key == 54:
+                    self.ans += '6'
+                elif key == 55:
+                    self.ans += '7'
+                elif key == 56:
+                    self.ans += '8'
+                elif key == 57:
+                    self.ans += '9'
+
+            if key == arcade.key.SPACE:
+                self.sum_ans = int(self.ans)
+                self.ans = ''
+                self.ans_pos_x = self.width/2
 
     def on_key_release(self, key, key_modifiers):
         if key == arcade.key.SPACE and self.state == 'start':
